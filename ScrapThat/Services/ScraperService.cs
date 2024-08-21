ba8ad12ba6142364c8209862fc6963dee715ffc4
bclaura <124773748+bclaura@users.noreply.github.com>
2024-08-21 11:39:43 +0300

    - fixed infinite loop bug by adding a comparison between pages -known bug - from too many requests a captcha will end all processing of products

--- ScrapThat/Services/ScraperService.cs
+++ ScrapThat/Services/ScraperService.cs
@@ -21,15 +21,31 @@ namespace ScrapThat.Services
 
         public async Task ScrapeProducts()
         {
-            int pageNumber = 1;
-            bool hasNextPage = true;
             var urls = File.ReadAllLines("urls.txt");
 
             foreach (var url in urls)
             {
-                while(hasNextPage)
+                int pageNumber = 1;
+                bool hasNextPage = true;
+
+                while (hasNextPage)
                 {
                     var links = $"{url}/p{pageNumber}/c";
+
+                    //workaround to be able to compare first page with finalUrl
+                    if(pageNumber == 1)
+                    {
+                        links = $"{url}/c";
+                    }
+
+                    var headResponse = await _httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Head, links));
+                    var finalUrl = headResponse.RequestMessage.RequestUri.ToString();
+
+                    if (!finalUrl.Equals(links, StringComparison.OrdinalIgnoreCase))
+                    {
+                        break;
+                    }
+
                     var web = new HtmlWeb();
                     var doc = await web.LoadFromWebAsync(links);
                     var products = doc.DocumentNode.SelectNodes("//div[@class='card-item card-standard js-product-data js-card-clickable ']");
@@ -42,151 +58,147 @@ namespace ScrapThat.Services
 
                     await ScrapeWebsiteAsync(links);
                     pageNumber++;
-                    if(pageNumber == 100)
-                    {
-
-                    }
                 }
             }
         }
 
         public async Task ScrapeWebsiteAsync(string url)
         {
+
             var web = new HtmlWeb();
-            var doc = await web.LoadFromWebAsync(url);
-            var count = 0;
 
-            var products = doc.DocumentNode.SelectNodes("//div[@class='card-item card-standard js-product-data js-card-clickable ']");
+                var doc = await web.LoadFromWebAsync(url);
 
-            if (products != null)
-            {
-                foreach (var product in products)
+                var products = doc.DocumentNode.SelectNodes("//div[@class='card-item card-standard js-product-data js-card-clickable ']");
+
+                if (products != null)
                 {
-                    count++;
-
-                    var productIdString = product.GetAttributeValue("data-product-id", string.Empty);
-                    int productId;
-
-                    if (int.TryParse(productIdString, out productId))
+                    foreach (var product in products)
                     {
-                        Product? existingProduct = null;
-                        var name = product.GetAttributeValue("data-name", string.Empty);
-                        var priceNode = product.SelectSingleNode(".//p[@class='product-new-price']");
-                        double price = 0;
-                        var image = string.Empty;
 
-                        var titleNode = product.SelectSingleNode(".//h2[@class='card-v2-title-wrapper']//a");
-                        var titleText = titleNode?.InnerText.Trim();
+                        var productIdString = product.GetAttributeValue("data-product-id", string.Empty);
+                        int productId;
 
-                        if(titleText != null && titleText.Contains("RESIGILAT", StringComparison.OrdinalIgnoreCase))
+                        if (int.TryParse(productIdString, out productId))
                         {
-                            continue;
-                        }
+                            Product? existingProduct = null;
+                            var name = product.GetAttributeValue("data-name", string.Empty);
+                            var priceNode = product.SelectSingleNode(".//p[@class='product-new-price']");
+                            double price = 0;
+                            var image = string.Empty;
 
-                        if (priceNode == null)
-                        {
-                            var otherPriceNode = product.SelectSingleNode(".//p[@class='product-new-price unfair-price']");
-                            var otherIntegerPartNode = otherPriceNode.SelectSingleNode(".//text()[normalize-space()]");
-                            var otherDecimalPartNode = otherPriceNode.SelectSingleNode(".//sup");
+                            var titleNode = product.SelectSingleNode(".//h2[@class='card-v2-title-wrapper']//a");
+                            var titleText = titleNode?.InnerText.Trim();
 
-                            var priceText = HttpUtility.HtmlDecode(otherPriceNode.InnerText);
-                            if(priceText.Contains("de la"))
+                            if (titleText != null && titleText.Contains("RESIGILAT", StringComparison.OrdinalIgnoreCase))
                             {
-
-                            }
-                            var match = Regex.Match(priceText, @"\d+(\.\d+)?");
-
-                            var integerPart = match.Value.Replace(".", string.Empty);
-                            var decimalPart = HttpUtility.HtmlDecode(otherDecimalPartNode?.InnerText.Trim());
-                            var noCommadecimalPart = decimalPart.Replace(",", string.Empty).Trim();
-                            var priceString = $"{integerPart}.{noCommadecimalPart}";
-                            price = double.Parse(priceString, CultureInfo.InvariantCulture);
-
-                        }
-                        else
-                        {
-                            var integerPartNode = priceNode.SelectSingleNode(".//text()[normalize-space()]");
-                            var decimalPartNode = priceNode.SelectSingleNode(".//sup");
-                            var integerPart = HttpUtility.HtmlDecode(integerPartNode?.InnerText.Trim()).Replace(".", string.Empty).Trim(); ;
-                            var decimalPart = HttpUtility.HtmlDecode(decimalPartNode?.InnerText.Trim());
-                            var noCommadecimalPart = decimalPart.Replace(",", string.Empty).Trim();
-                            var priceString = $"{integerPart}.{noCommadecimalPart}";
-                            if (priceString.Contains("de la"))
-                            {
-
+                                continue;
                             }
 
-                            price = double.Parse(priceString, CultureInfo.InvariantCulture);
-                        }
-
-
-                        var currency = "Lei";
-
-                        var imageNode = product.SelectSingleNode(".//div[contains(@class, 'img-component')]//img");
-                        if(imageNode != null )
-                        {
-                            image = imageNode.GetAttributeValue("src", string.Empty);
-                        }
-                        else
-                        {
-                            var bundleImageNode = product.SelectSingleNode(".//div[contains(@class, 'bundle-image')]");
-                            if (bundleImageNode != null)
+                            if (priceNode == null)
                             {
-                                var styleAttribute = bundleImageNode.GetAttributeValue("style", string.Empty);
-                                var urlPattern = @"url\((?<url>[^)]+)\)";
-                                var matches = Regex.Matches(styleAttribute, urlPattern);
+                                var otherPriceNode = product.SelectSingleNode(".//p[@class='product-new-price unfair-price']");
+                                var otherIntegerPartNode = otherPriceNode.SelectSingleNode(".//text()[normalize-space()]");
+                                var otherDecimalPartNode = otherPriceNode.SelectSingleNode(".//sup");
 
-                                foreach (Match match in matches)
+                                var priceText = HttpUtility.HtmlDecode(otherPriceNode.InnerText);
+                                if (priceText.Contains("de la"))
                                 {
-                                    var imageUrl = match.Groups["url"].Value;
-                                    image += imageUrl + "; ";
-                                }
-                                image = image.TrimEnd(';', ' ');
-                            }
-                        }
-                        
 
-                        existingProduct = await _context.Products.FirstOrDefaultAsync(p => p.Id == productId);
-                        if (existingProduct == null)
-                        {
-                            existingProduct = new Product
+                                }
+                                var match = Regex.Match(priceText, @"\d+(\.\d+)?");
+
+                                var integerPart = match.Value.Replace(".", string.Empty);
+                                var decimalPart = HttpUtility.HtmlDecode(otherDecimalPartNode?.InnerText.Trim());
+                                var noCommadecimalPart = decimalPart.Replace(",", string.Empty).Trim();
+                                var priceString = $"{integerPart}.{noCommadecimalPart}";
+                                price = double.Parse(priceString, CultureInfo.InvariantCulture);
+
+                            }
+                            else
                             {
-                                Name = name,
-                                Currency = currency,
-                                Image = image,
-                                DateChecked = DateTime.Now
-                            };
-                            _context.Products.Add(existingProduct);
+                                var integerPartNode = priceNode.SelectSingleNode(".//text()[normalize-space()]");
+                                var decimalPartNode = priceNode.SelectSingleNode(".//sup");
+                                var integerPart = HttpUtility.HtmlDecode(integerPartNode?.InnerText.Trim()).Replace(".", string.Empty).Trim(); ;
+                                var decimalPart = HttpUtility.HtmlDecode(decimalPartNode?.InnerText.Trim());
+                                var noCommadecimalPart = decimalPart.Replace(",", string.Empty).Trim();
+                                var priceString = $"{integerPart}.{noCommadecimalPart}";
+                                if (priceString.Contains("de la"))
+                                {
+
+                                }
+
+                                price = double.Parse(priceString, CultureInfo.InvariantCulture);
+                            }
+
+
+                            var currency = "Lei";
+
+                            var imageNode = product.SelectSingleNode(".//div[contains(@class, 'img-component')]//img");
+                            if (imageNode != null)
+                            {
+                                image = imageNode.GetAttributeValue("src", string.Empty);
+                            }
+                            else
+                            {
+                                var bundleImageNode = product.SelectSingleNode(".//div[contains(@class, 'bundle-image')]");
+                                if (bundleImageNode != null)
+                                {
+                                    var styleAttribute = bundleImageNode.GetAttributeValue("style", string.Empty);
+                                    var urlPattern = @"url\((?<url>[^)]+)\)";
+                                    var matches = Regex.Matches(styleAttribute, urlPattern);
+
+                                    foreach (Match match in matches)
+                                    {
+                                        var imageUrl = match.Groups["url"].Value;
+                                        image += imageUrl + "; ";
+                                    }
+                                    image = image.TrimEnd(';', ' ');
+                                }
+                            }
+
+
+                            existingProduct = await _context.Products.FirstOrDefaultAsync(p => p.Id == productId);
+                            if (existingProduct == null)
+                            {
+                                existingProduct = new Product
+                                {
+                                    Name = name,
+                                    Currency = currency,
+                                    Image = image,
+                                    DateChecked = DateTime.Now
+                                };
+                                _context.Products.Add(existingProduct);
+                                await _context.SaveChangesAsync();
+                            }
+
+
+                            var today = DateTime.Today;
+
+                            var existingPriceHistory = await _context.ProductPriceHistories
+                                        .FirstOrDefaultAsync(p => p.ProductId == existingProduct.Id && p.DateChecked == today);
+
+                            if (existingPriceHistory != null)
+                            {
+                                existingPriceHistory.Price = price;
+                            }
+                            else
+                            {
+                                var productPriceHistory = new ProductPriceHistory
+                                {
+                                    ProductId = existingProduct.Id,
+                                    Price = price,
+                                    DateChecked = DateTime.Now
+                                };
+
+                                _context.ProductPriceHistories.Add(productPriceHistory);
+                            }
+
                             await _context.SaveChangesAsync();
                         }
-
-
-                        var today = DateTime.Today;
-
-                        var existingPriceHistory = await _context.ProductPriceHistories
-                                    .FirstOrDefaultAsync(p => p.ProductId == existingProduct.Id && p.DateChecked == today);
-
-                        if (existingPriceHistory != null)
-                        {
-                            existingPriceHistory.Price = price;
-                        }
-                        else
-                        {
-                            var productPriceHistory = new ProductPriceHistory
-                            {
-                                ProductId = existingProduct.Id,
-                                Price = price,
-                                DateChecked = DateTime.Now
-                            };
-
-                            _context.ProductPriceHistories.Add(productPriceHistory);
-                        }
-
-                        await _context.SaveChangesAsync();
                     }
-                }
 
-            }
+                }
         }
     }
 }
